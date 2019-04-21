07d45dc18371dcfb2b96f0dc8a0fde90f5a36262
Wendy Huynh <wendyhuynhs@gmail.com>
2019-04-21 15:38:09 -0700

    Updated room code

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,3 +1,16 @@
 var Rooms = {
-
+  roomsList: [],
+  add: function(room) {
+    if (typeof room === 'object') {
+      if (!Rooms.roomsList.includes(room.roomname)) {
+        RoomsView.renderRoom(room);
+        Rooms.roomsList.push(room.roomname);
+      }
+    } else {
+      if (!Rooms.roomsList.includes(room)) {
+        Rooms.roomsList.push(room);
+        RoomsView.renderRoom(room);
+      }
+    }
+  }
 };
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -1,26 +1,23 @@
 var RoomsView = {
-
   $button: $('#rooms button'),
   $select: $('#rooms select'),
 
   initialize: function() {
-    //render rooms that are in select
-    // for (var i = 0; i < Messages.result.length; i++) {
-      RoomsView.$select.append(RoomsView.renderRoom())
-    // }
+    RoomsView.$button.on('click', function() {
+      Rooms.add($('#room').val());
+    });
+    RoomsView.$select.append(RoomsView.renderRoom());
   },
-
 
   renderRoom: function(data) {
     // if (data.roomname === undefined) {
     //   data.roomname = '';
     // }
-  //  return RoomsView.(data.roomname)
-  // }
-  RoomsView.$button.on('click', function () {
-    var input = $('input:text').val()
-      RoomsView.$select.append(`<option value="${input}" > ${input} </option>`)
-  })
+    //  return RoomsView.(data.roomname)
+    // }
+    RoomsView.$button.on('click', function() {
+      var input = $('input:text').val();
+      RoomsView.$select.append(`<option value="${input}" > ${input} </option>`);
+    });
   }
-
 };
